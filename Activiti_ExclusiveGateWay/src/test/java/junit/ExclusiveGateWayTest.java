8d6d7c7fb72e25071da59b3c4bdcc01207d2fe3b
startcaft <51418996@qq.com>
2016-02-01 16:20:54 +0800

    一个排他网关对应一个以上的顺序流。 由排他网关流出的顺序流都有个condition，在内部维护返回boolean类型的决策结果。 排他网关只会返回一个结果。当流程执行到排他网关时，流程引擎自动检测网关出口，从上向下检索，如果发现第一条决策为true或者没有设置条件(默认为成立)，则流出。
    如果没有任何一个出口符合条件，则抛出异常。
    
    使用流程变量，设置连线的条件，并按照连线的条件执行工作流，如果没有条件符合的，则以默认的连线离开。

---
+++ Activiti_ExclusiveGateWay/src/test/java/junit/ExclusiveGateWayTest.java
@@ -0,0 +1,88 @@
+package junit;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.junit.Test;
+
+public class ExclusiveGateWayTest {
+	
+	private ProcessEngine engine;
+
+	{
+		engine = ProcessEngines.getDefaultProcessEngine();
+	}
+	
+	/** 部署流程定义 **/
+	@Test
+	public void deploymentTest() {
+
+		Deployment deploy = engine.getRepositoryService().createDeployment()
+				.name("排他网关").addClasspathResource("diagrams/exclusiveGateWay.bpmn")
+				.addClasspathResource("diagrams/exclusiveGateWay.png").deploy();
+		
+		System.out.println("部署ID：" + deploy.getId());
+		System.out.println("部署名称：" + deploy.getName());
+	}
+	
+	/** 启动一个流程实例 **/
+	@Test
+	public void startProcessInstanceTest(){
+		
+		String processDefinitionKey = "exclusiveGateWay";
+		
+		ProcessInstance processInstance = engine.getRuntimeService().startProcessInstanceByKey(processDefinitionKey);
+		
+		System.out.println("流程实例ID：" + processInstance.getId());
+		System.out.println("流程定义ID：" + processInstance.getProcessDefinitionId());
+	}
+	
+	/** 查询当前人的个人任务 **/
+	@Test
+	public void findMyPersonTask(){
+		
+		//String assignee = "王小五";
+		String assignee = "赵小六";
+		
+		List<Task> list = engine.getTaskService()			//与正在执行的任务管理相关的Service
+										.createTaskQuery()			//创建任务查询对象
+										.taskAssignee(assignee)		//指定个人任务查询，指定办理人
+										.list();
+		
+		if (list != null && list.size() > 0) {
+			for (Task task : list) {
+				System.out.println("任务ID：" + task.getId());
+				System.out.println("任务名称：" + task.getName());
+				System.out.println("任务的创建时间：" + task.getCreateTime());
+				System.out.println("任务的办理人：" + task.getAssignee());
+				System.out.println("流程实例ID：" + task.getProcessInstanceId());
+				System.out.println("执行对象ID：" + task.getExecutionId());
+				System.out.println("流程定义ID：" + task.getProcessDefinitionId());
+				System.out.println("#######################################################################");
+			}
+		}
+	}
+	
+	
+	/** 完成任务 **/
+	@Test
+	public void completeMyPersonTask(){
+		
+		//任务ID
+		String taskId = "70004";
+		
+		//完成任务的同时，设置流程变量，使用流程变量用于指定完成后，下一个连线，对应.bpmn文件中的${money>=500 && money <1000'}
+		Map<String,Object> variables = new HashMap<String, Object>();
+		variables.put("money", 800);
+		engine.getTaskService()
+				.complete(taskId, variables);
+		
+		System.out.println("完成任务：任务ID：" + taskId);
+	}
+}
